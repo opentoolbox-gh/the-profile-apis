31a8a50993dd010a474c1adbf9d26fad82fe1960
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2023-11-29 09:45:50 +0200

    chore: project initialization && basic setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # the-profile-apis
+
 APIs that we'll be using to test a react app with cypress + jest.
---
+++ src/main/java/theprofiler/vercel/app/apis/ApisApplication.java
@@ -0,0 +1,13 @@
+package theprofiler.vercel.app.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisApplication.class, args);
+	}
+
+}
